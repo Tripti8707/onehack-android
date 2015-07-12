1a475b5fbc7aed7c6122deff4d2906ac4d00cee1
Omkar Moghe <omoghe@umich.edu>
2015-07-12 15:35:09 -0700

    event manager, index builder for dynamic page creation

---
+++ app/src/main/java/com/arbrr/onehack/ui/events/DayPagerAdapter.java
@@ -0,0 +1,31 @@
+package com.arbrr.onehack.ui.events;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Omkar Moghe on 7/12/2015.
+ */
+public class DayPagerAdapter extends FragmentStatePagerAdapter {
+
+    public DayPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        if (EventsManager.buildIndexes() != null) {
+            ArrayList<Integer> indexes = EventsManager.buildIndexes().get(i);
+            return DayFragment.newInstance(indexes.get(0), indexes.get(1));
+        } else return new DayFragment();
+    }
+
+    @Override
+    public int getCount() {
+        if (EventsManager.buildIndexes() != null) return EventsManager.buildIndexes().size();
+        else return 0;
+    }
+}
---
+++ app/src/main/java/com/arbrr/onehack/ui/events/EventsManager.java
@@ -0,0 +1,76 @@
+package com.arbrr.onehack.ui.events;
+
+import com.arbrr.onehack.data.model.Event;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Omkar Moghe on 7/12/2015.
+ */
+public class EventsManager {
+    private static ArrayList<Event> events;
+
+    public static ArrayList<Event> getEvents() {
+        return events;
+    }
+
+    public static ArrayList<ArrayList<Integer>> buildIndexes() {
+
+        if (!isNull() && events.size() > 0) {
+
+            ArrayList<ArrayList<Integer>> indexMap = new ArrayList<>();
+
+            int index = 0;
+            int start = 0;
+            int end = 0;
+            int lastDate = events.get(0).getStartTime().getDate();
+
+            for (Event e : events) {
+                if (e.getStartTime().getDate() == lastDate) {
+                    ++end;
+                } else {
+                    ArrayList<Integer> arr = new ArrayList<>();
+                    arr.add(start);
+                    arr.add(end);
+                    indexMap.set(index, arr);
+
+                    start = end;
+                    lastDate = e.getStartTime().getDate();
+                    ++index;
+                }
+            }
+
+            return indexMap;
+        }
+
+        return null;
+    }
+
+    public static ArrayList<Event> getEvents(int start) {
+        if (!isNull()) return new ArrayList<Event>(events.subList(start, events.size()));
+        else return new ArrayList<Event>();
+    }
+
+    public static ArrayList<Event> getEvents(int start, int end) {
+        if (!isNull()) return new ArrayList<Event>(events.subList(start, end));
+        else return new ArrayList<Event>();
+    }
+
+    public static void setEvents(ArrayList<Event> events) {
+        EventsManager.events = events;
+    }
+
+    public static void setEvents(List<Event> events) {
+        EventsManager.events = new ArrayList<Event>(events);
+    }
+
+    public static boolean isNull() {
+        return (events == null);
+    }
+
+    public static int getNumEvents() {
+        if (!isNull()) return events.size();
+        else return -1;
+    }
+}
